5afc4dfa0354a185bf699564106d48d1bb3af3a2
unknown <rayman.legens12@gmail.com>
2022-03-03 15:04:08 +0200

    Fixed logic of hierarchy

--- tests/test_parse_medical_data.py
+++ tests/test_parse_medical_data.py
@@ -3,26 +3,40 @@ from parse_medical_data.read_medical_data import ReadMedicalData
 def main():
     medical_data = ReadMedicalData().get_medical_data()
 
-    # button = "Start"
-    button = "Серцево-легенева реанімація"
-    #button = "Так"
-    # button = "Дитина 1-8 років"
-    #button = "Back"
+    #Нажато страт
+    medical_data.init_begin_level()
 
-    if button == "Start":
-        begin_options = medical_data.get_begin_options()
-        print(begin_options)
-    elif button == "Back":
-        back_options = medical_data.get_back_options()
-        back_answer = medical_data.get_back_answer()
-        back_link = medical_data.get_back_link()
-    else:
-        next_options = medical_data.get_next_options(button)
-        answer = medical_data.get_answer(button)
-        link = medical_data.get_link(button)
-        print(next_options)
-        print(answer)
-        print(link)
+    # Функціонал кнопки
+    begin_options = medical_data.get_begin_options()
+    #begin_options містить кнопки першого рівня
+
+    #Нажато трерю кнопку "Кровотеча"
+    third_button = begin_options[3]
+
+    #Функціонал кнопки
+    options = medical_data.get_options(third_button)
+    # options містить кнопки рівня "Кровотеча"
+    answer = medical_data.get_answer(third_button)
+    link = medical_data.get_link(third_button)
+
+    # Нажато кнопку "Так"
+    button = options[0]
+
+    # Функціонал кнопки
+    options = medical_data.get_options(button)
+    # options містить кнопки рівня "Так"
+    answer = medical_data.get_answer(button)
+    link = medical_data.get_link(button)
+
+    # Назад 1
+    back_options = medical_data.get_back_options()
+    back_answer = medical_data.get_back_answer()
+    back_link = medical_data.get_back_link()
+
+    # Назад 2
+    back_options = medical_data.get_back_options()
+    back_answer = medical_data.get_back_answer()
+    back_link = medical_data.get_back_link()
 
     break_point = 1
 
