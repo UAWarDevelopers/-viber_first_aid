11418ed7855e816cb3c6265d0e034313ab7ba93c
vityha <viktor.povazhuk@ucu.edu.ua>
2022-03-03 13:34:40 +0200

    1. some comments in test or med data

--- tests/test_parse_medical_data.py
+++ tests/test_parse_medical_data.py
@@ -4,20 +4,26 @@ def main():
     medical_data = ReadMedicalData().get_medical_data()
 
     # button = "Start"
-    button = "Серцево-легенева реанімація"
+    # button = "Серцево-легенева реанімація"
     #button = "Так"
     # button = "Дитина 1-8 років"
-    #button = "Back"
+    button = "Back"
+    try_back_option = "Підвищення цукру в крові"
+
 
     if button == "Start":
         begin_options = medical_data.get_begin_options()
         print(begin_options)
     elif button == "Back":
+        next_options = medical_data.get_answer(try_back_option)
         back_options = medical_data.get_back_options()
+        # return only on 1 level
         back_answer = medical_data.get_back_answer()
         back_link = medical_data.get_back_link()
     else:
         next_options = medical_data.get_next_options(button)
+        # answer from bot
+        # in table - column 'answer'
         answer = medical_data.get_answer(button)
         link = medical_data.get_link(button)
         print(next_options)
@@ -28,3 +34,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# is it all functions?
+# yes, all public
+# 
+
+# TODO: 1. get_next_element
